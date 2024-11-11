97fba6702bf5792890b8072903dee7d9098fa5cc
chenhao91 <chenhao91@meituan.com>
2024-11-11 17:10:26 +0800

    doc

--- components/blog/post-list.tsx
+++ components/blog/post-list.tsx
@@ -9,22 +9,28 @@ type Props = {
   posts: Post[]
 }
 
+/**
+ * PostList 组件用于展示一系列的文章预览和一个侧边栏的热门文章。
+ * @param {Props} props - 包含文章列表的属性。
+ */
 function PostList({ posts }: Props) {
   return (
     <section>
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
         <div className="pt-32 pb-12 md:pt-40 md:pb-20">
 
-          {/* Page header */}
+          {/* 页面头部 */}
           <div className="max-w-3xl pb-12 md:pb-20 text-center md:text-left">
             <h1 className="h1 mb-4">Explore my notes</h1>
-            <p className="text-xl text-gray-600">Posts are connected through <PreviewLink href="/notes/bi-directional-links">bi-directional links</PreviewLink>. Click any post and check it out!</p>
+            <p className="text-xl text-gray-600">
+              Posts are connected through <PreviewLink href="/notes/bi-directional-links">bi-directional links</PreviewLink>. Click any post and check it out!
+            </p>
           </div>
 
-          {/* Main content */}
+          {/* 主要内容 */}
           <div className="md:flex md:justify-between">
 
-            {/* Articles container */}
+            {/* 文章容器 */}
             <div className="md:grow -mt-4">
               {posts.map((post) => (
                 <PostPreview
@@ -38,7 +44,7 @@ function PostList({ posts }: Props) {
               ))}
             </div>
 
-            {/* Sidebar */}
+            {/* 侧边栏 */}
             <aside className="relative mt-12 md:mt-0 md:w-64 md:ml-12 lg:ml-20 md:shrink-0">
               <PopularPosts />
             </aside>
@@ -50,5 +56,7 @@ function PostList({ posts }: Props) {
     </section>
   );
 }
+
+
 
 export default PostList;
