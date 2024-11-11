414d6ced9ac8fad1b2ed2101376837501a4f520a
chenhao91 <chenhao91@meituan.com>
2024-11-11 17:11:50 +0800

    doc

--- components/blog/pagination.tsx
+++ components/blog/pagination.tsx
@@ -3,21 +3,41 @@ type Props = {
   maxPage: number
 }
 
+/**
+ * 生成分页数组
+ * @param currPage 当前页码
+ * @param maxPage 最大页码
+ * @param numItems 分页显示的页码数量，默认为5
+ * @returns 分页数组
+ */
 function getPaginationArr(currPage: number, maxPage: number, numItems: number = 5) {
-  const pageList = [currPage]
+  // 初始化分页数组，包含当前页码
+  const pageList = [currPage];
+  
+  // 循环填充分页数组，直到达到指定的页码数量
   while (pageList.length < numItems) {
+    // 计算当前页码与数组最左边页码的差值
     const leftDelta = Math.abs(pageList[0] - currPage);
+    // 计算当前页码与数组最右边页码的差值
     const rightDelta = Math.abs(pageList[pageList.length - 1] - currPage);
+    
+    // 根据差值和边界条件决定向左或向右扩展页码
     if ((leftDelta <= rightDelta || pageList[pageList.length - 1] == maxPage) && pageList[0] > 1) {
+      // 向左扩展页码
       pageList.unshift(pageList[0] - 1);
     } else if ((leftDelta > rightDelta || pageList[0] == 1) && pageList[pageList.length - 1] < maxPage) {
+      // 向右扩展页码
       pageList.push(pageList[pageList.length - 1] + 1);
     } else {
+      // 如果无法继续扩展，跳出循环
       break;
     }
   }
+  
+  // 返回生成的分页数组
   return pageList;
 }
+
 
 function Pagination({ currPage, maxPage }: Props) {
   const paginationArr = getPaginationArr(currPage, maxPage);
