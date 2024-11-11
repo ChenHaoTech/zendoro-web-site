e3ee02ddba4f3545158e0cba96cc85fa5e9ef2f1
chenhao91 <chenhao91@meituan.com>
2024-11-11 17:28:56 +0800

    quick fix

--- next.config.js
+++ next.config.js
@@ -2,15 +2,15 @@ module.exports = {
   async redirects() {
     return [
       {
-        source: '/posts',
-        destination: '/posts/1',
+        source: "/posts",
+        destination: "/posts/1",
         permanent: true,
       },
       {
-        source: '/docs',
-        destination: '/docs/getting-started/introduction',
+        source: "/docs",
+        destination: "/docs/Getting Started/Start here",
         permanent: true,
       },
-    ]
+    ];
   },
-}
+};
