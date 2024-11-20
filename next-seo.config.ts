02a0e21fcd567b74747051d3893e18d686333282
chenhao91 <chenhao91@meituan.com>
2024-11-20 15:05:41 +0800

    update icon

--- next-seo.config.ts
+++ next-seo.config.ts
@@ -29,24 +29,24 @@ const config: DefaultSeoProps = {
     {
       rel: 'icon',
       type: 'image/png',
-      href: '/favicon/32.png',
+      href: '/favicon/favicon-32x32.png',
       sizes: '32x32'
     },
     {
       rel: 'icon',
       type: 'image/png',
-      href: '/favicon/16.png',
+      href: '/favicon/favicon-16x16.png',
       sizes: '16x16'
     },
     {
       rel: "apple-touch-icon",
       sizes: "180x180",
-      href: "/favicon/180.png"
+      href: "/apple-touch-icon.png"
     },
     {
       rel: "shortcut icon",
       type: "image/png",
-      href: "/favicon/196.png"
+      href: "/favicon.ico"
     },
     {
       rel: "manifest",
